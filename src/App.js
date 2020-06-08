9a49db0cede074d9476c45b9fef6b150003aef03
anshul shrivastava <anshulshrivastava174@gmail.com>
2020-06-08 16:58:04 +0530

    links identified added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import HomePage from '../src/pages/homepage/homepage.component'
 import './App.css';
+import {Switch,Route} from 'react-router-dom'
 
+const HatsPage = () =>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Switch>
+      <Route exact path ='/' component={HomePage}/>
+      <Route exact path ='/hats' component={HatsPage}/>
+    </Switch>
   );
 }
 
